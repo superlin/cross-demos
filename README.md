7744f647905017622fdf773e88ebbaac137d3603
superlin <1092793900@qq.com>
2015-04-16 23:49:26 +0800

    增加SSE demos

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ##跨域通信demos
-###图像ping
-###JSONP
+*	图像ping
+*	JSONP
+*	Comet - Long Polling
+*	Comet - Http Streaming
+*	Server-sent Events
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'cors示例' });
 });
 
+/*图片ping与jsonp页面*/
 router.get('/ping-jsonp', function(req, res, next) {
   res.render('ping-jsonp', { title: 'image ping & jsonp' });
 });
 
+/*图片ping请求*/
 router.get('/img', function(req, res, next) {
   res.send('我是一张图片，我说这句话好像没什么用');
 });
 
+/*jsonp请求*/
 router.get('/jsonp', function(req, res, next) {
   res.send('pagefunc(' + Math.random() + ')');
 });
 
+/*comet页面*/
 router.get('/comet', function(req, res, next) {
   res.render('comet', { title: 'comet' });
 });
 
+/*长轮询*/
 router.get('/poll/:id', function(req, res, next) {
   res.writeHead(200, {
     'Access-Control-Allow-Origin': '*'
@@ -31,6 +36,7 @@ router.get('/poll/:id', function(req, res, next) {
   }, 1000);
 });
 
+/*http stream*/
 router.get('/stream', function(req, res, next) {
 	res.writeHead(200, {
     'Access-Control-Allow-Origin': '*',
@@ -44,6 +50,32 @@ router.get('/stream', function(req, res, next) {
   		res.end();
   	}
   }, 1000);
+});
+
+/*sse页面*/
+router.get('/sse', function(req, res, next) {
+  res.render('sse', { title: 'sse' });
+});
+
+/*sse连接*/
+router.get('/event-stream', function(req, res, next) {
+  res.writeHead(200, {
+    "Content-Type":"text/event-stream",
+    "Cache-Control":"no-cache",
+    "Connection":"keep-alive",
+    "Access-Control-Allow-Origin" : "*"
+  });
+  res.write("retry: 10000\n");
+  res.write("event: connecttime\n");
+  res.write("data: " + (new Date()) + "\n\n");
+  res.write("data: " + (new Date()) + "\n\n");
+
+  var interval = setInterval(function() {
+    res.write("data: " + (new Date()) + "\n\n");
+  }, 1000);
+  req.connection.addListener("close", function () {
+    clearInterval(interval);
+  }, false);
 });
 
 module.exports = router;
